5ec61482a26c9cde07c61cc956d5723762b2470e
huaiyinfeilong <huaiyinfeilong@163.com>
2015-10-31 09:33:34 +0800

    更新代码。

--- weather/gen/com/xysoftlab/android/weather/R.java
+++ weather/gen/com/xysoftlab/android/weather/R.java
@@ -25,7 +25,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
+        public static final int action_settings=0x7f080007;
+        public static final int day1=0x7f080002;
+        public static final int day2=0x7f080003;
+        public static final int day3=0x7f080004;
+        public static final int day4=0x7f080005;
+        public static final int day5=0x7f080006;
+        public static final int day_group=0x7f080001;
         public static final int weather_details=0x7f080000;
     }
     public static final class layout {
